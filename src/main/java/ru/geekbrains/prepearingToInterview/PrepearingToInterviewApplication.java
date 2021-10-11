84fbacd8762f46513d505392c2121d7045f23611
biglov_dr <simondo@inbox.ru>
2021-10-11 16:44:46 +0300

    initcommit

---
+++ src/main/java/ru/geekbrains/prepearingToInterview/PrepearingToInterviewApplication.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.prepearingToInterview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrepearingToInterviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrepearingToInterviewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/geekbrains/prepearingToInterview/PrepearingToInterviewApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.prepearingToInterview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrepearingToInterviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
